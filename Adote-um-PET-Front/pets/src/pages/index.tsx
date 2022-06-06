823bc9917c843e253a744e0f1f502fbc785254d5
Aslay Clevisson <aslayclei@gmail.com>
2022-06-06 18:53:00 -0300

    Caixa de diálogo e de mensssagem durante a adoção

--- Adote-um-PET-Front/pets/src/pages/index.tsx
+++ Adote-um-PET-Front/pets/src/pages/index.tsx
@@ -1,6 +1,13 @@
 import type { NextPage } from "next";
 import Titulo from "../ui/components/Titulo/Titulo";
 import Lista from "../ui/components/Lista/Lista";
+import {
+  Dialog,
+  TextField,
+  DialogActions,
+  Button,
+  Snackbar,
+} from "@mui/material";
 
 const Home: NextPage = () => {
   return (
@@ -15,24 +22,42 @@ const Home: NextPage = () => {
             </span>
           }
         />
-      </div>
 
-      <Lista
-        pets={[
-          {
-            id: 1,
-            nome: "Bobby",
-            historia: "Um cachorro muito bem cuidado",
-            foto: "https://veja.abril.com.br/wp-content/uploads/2017/01/cao-labrador-3-copy.jpg",
-          },
-          {
-            id: 2,
-            nome: "Scobby",
-            historia: "Cachorro cheiroso e alegre",
-            foto: "https://veja.abril.com.br/wp-content/uploads/2017/01/cao-labrador-3-copy.jpg",
-          },
-        ]}
-      />
+        <Lista
+          pets={[
+            {
+              id: 1,
+              nome: "Bobby",
+              historia:
+                "Um cachorro muito bem cuidado e...... Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi ullam ex ducimus, deleniti architecto, saepe eligendi consequatur amet, atque perferendis quod unde quae! Ipsa magni fugit debitis omnis consequatur rem!",
+              foto: "https://veja.abril.com.br/wp-content/uploads/2017/01/cao-labrador-3-copy.jpg",
+            },
+            {
+              id: 2,
+              nome: "Scobby",
+              historia: "Cachorro cheiroso e alegre",
+              foto: "https://veja.abril.com.br/wp-content/uploads/2017/01/cao-labrador-3-copy.jpg",
+            },
+          ]}
+        />
+        {/* sx viabiliza o uso do css */}
+        <Dialog open={false} fullWidth PaperProps={{ sx: { padding: "40px" } }}>
+          <TextField
+            label={"E-mail"}
+            type={"email"}
+            sx={{ pb: "10px" }}
+            fullWidth
+          />
+          <TextField label={"Quantia por mês"} type={"number"} fullWidth />
+
+          <DialogActions>
+            <Button>Cancelar</Button>
+            <Button variant={"contained"}>Confirmar adoção</Button>
+          </DialogActions>
+        </Dialog>
+
+        <Snackbar open={false} message={"Você acaba de adotar um animal"} />
+      </div>
     </>
   );
 };
